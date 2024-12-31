9792f880b96f074268866bbdae15d31cadaa98af
nithish0302 <nithi2005palani@gmail.com>
2024-12-31 08:39:09 +0530

    File Modified

--- Todo.js
+++ Todo.js
@@ -5,17 +5,27 @@ document.addEventListener('DOMContentLoaded', (event) => {
 
 let maintainTask = JSON.parse(localStorage.getItem('task')) || [];
 
-let completedTask = [];
+let completedTask = JSON.parse(localStorage.getItem('completed')) || [];
 
 show();
 function addList() {
     let taskget = document.querySelector('.input_type_text').value;
+    if (!taskget) {
+        alert("Enter the TaskName");
+        return
+    }
+
     let dateget = document.querySelector('.input_type_date').value;
 
+    if (!dateget) {
+        alert("Select the date ");
+        return
+    }
     maintainTask.push({
         task: taskget,
         date: dateget
     });
+
 
     localStorage.setItem('task', JSON.stringify(maintainTask));
 
@@ -27,22 +37,60 @@ function addList() {
 
 function show() {
     let combine = '';
+
+    maintainTask.sort((a, b) => {
+        let dateOfA = new Date(a.date);
+        let dateOfB = new Date(b.date);
+        return dateOfA - dateOfB;
+    })
     maintainTask.forEach((tasks, index) => {
+
+        let dateObj = new Date(tasks.date);
+        let formattedDate =
+            String(dateObj.getDate()).padStart(2, '0') + '/' +
+            String(dateObj.getMonth() + 1).padStart(2, '0') + '/' +
+            dateObj.getFullYear();
         let html = `
             <div class="task-row">
                 <div class="task-name">${tasks.task}</div>
-                <div class="task-date">${tasks.date}</div>
+                <div class="task-date">${formattedDate}</div>
                 <div class="task-actions">
-                    <button ><img src="edit.png" alt="Edit" style="width: 20px; height: 20px;"></button>
-                    <button onclick=" complete(index)"><img src="accept.png" alt="Accept" style="width: 20px; height: 20px;"></button>
+                    <button onclick="edit(${index})"><img src="edit.png" alt="Edit" style="width: 20px; height: 20px;"></button>
+                    <button onclick="complete(${index})"><img src="accept.png" alt="Accept" style="width: 20px; height: 20px;"></button>
                 </div>
             </div>`;
-            
+
+
         combine += html;
     });
     document.querySelector('.tasks').innerHTML = combine;
 }
 
+function complete(index) {
+    const remove = maintainTask.splice(index, 1);
+    if (remove.length > 0) {
+        completedTask.push(remove[0]);
+    }
+
+    localStorage.setItem('task', JSON.stringify(maintainTask));
+    localStorage.setItem('completed', JSON.stringify(completedTask));
+    show();
+}
+let editindex = -1;
+function edit(index) {
+    let edittask = maintainTask[index];
+
+    document.querySelector('.edit-task-name').value = edittask.task;
+    document.querySelector('.edit-task-date').value = edittask.date;
+
+    editindex = index;
+    document.querySelector('.edit-popup').style.display = 'grid';
+
+}
+
+function close() {
+    document.querySelector('.popup').style.display = 'none';
+}
 
 
 
